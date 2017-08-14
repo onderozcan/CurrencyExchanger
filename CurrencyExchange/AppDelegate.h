40a945ea7b6ebb68a9a73d16224439fa1fcf974e
onderozcan <onder@pixelblind.net>
2017-08-14 20:12:24 +0100

    First initialisation

---
+++ CurrencyExchange/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CurrencyExchange
+//
+//  Created by Önder ÖZCAN on 14/08/2017.
+//  Copyright © 2017 pixelblind. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
